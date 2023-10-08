0e567082a8272744552403c618eb7f3f94ee4875
Vinicius Leonel <viniciuslps.cms@gmail.com>
2023-10-08 18:27:17 -0300

    finalizando projeto

--- src/app/components/card/card-price/card-price.component.ts
+++ src/app/components/card/card-price/card-price.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-card-price',
   templateUrl: './card-price.component.html',
   styleUrls: ['./card-price.component.css']
 })
-export class CardPriceComponent {
+export class CardPriceComponent implements OnInit{
+
+  @Input()
+  gameTitle:string = ""
+  @Input()
+  gameType:string = ""
+  @Input()
+  gamePrice:string = ""
+
+  ngOnInit(): void {
+  }
 
 }
--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.css']
 })
-export class CardComponent {
+export class CardComponent implements OnInit{
+
+  @Input()
+  gameTitle:string = ""
+  @Input()
+  gameCover:string = ""
+  @Input()
+  gameCoverAlt:string = ""
+  @Input()
+  gameLabel:string = ""
+  @Input()
+  gameType:string = ""
+  @Input()
+  gamePrice:string = ""
+
+  ngOnInit(): void {
+    }
 
 }
